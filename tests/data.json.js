2d1c336ceede986782b1aba24baf3195de947796
Roman Gubarenko <rgubarenko@gmail.com>
2013-06-06 00:37:18 -0400

    first commit

---
+++ tests/data.json.js
@@ -0,0 +1,72 @@
+﻿var data_AngularDB =
+{
+  projects:
+    [
+      {
+        "_id": { "$oid": "51873902e4b0072de7e07c37" },
+        "name": "SproutCore",
+        "site": "http://sproutcore.com/",
+        "description": "Innovative web-apps."
+      },
+      {
+        "_id": { "$oid": "51873902e4b0072de7e07c38" },
+        "name": "Backbone",
+        "site": "http://documentcloud.github.com/backbone/",
+        "description": "Models for your apps."
+      },
+      {
+        "_id": { "$oid": "51873902e4b0072de7e07c39" },
+        "name": "jQuery",
+        "site": "http://jquery.com/",
+        "description": "Write less, do more."
+      },
+      {
+        "_id": { "$oid": "51873902e4b00e8a821a9059" },
+        "name": "Sammy",
+        "site": "http://sammyjs.org/",
+        "description": "Small with class."
+      },
+      {
+        "_id": { "$oid": "51873902e4b00e8a821a905a" },
+        "name": "AngularJS",
+        "site": "http://angularjs.org",
+        "description": "HTML enhanced for web apps!"
+      },
+      {
+        "_id": { "$oid": "51873903e4b00e8a821a905b" },
+        "name": "Spine",
+        "site": "http://spinejs.com/",
+        "description": "Awesome MVC Apps."
+      },
+      {
+        "_id": { "$oid": "51873903e4b0072de7e07c3a" },
+        "name": "Knockout",
+        "site": "http://knockoutjs.com/",
+        "description": "MVVM pattern."
+      },
+      {
+        "_id": { "$oid": "51873903e4b0072de7e07c3b" },
+        "name": "Cappucino",
+        "site": "http://cappuccino.org/",
+        "description": "Objective-J."
+      },
+      {
+        "_id": { "$oid": "51873903e4b00e8a821a905c" },
+        "name": "GWT",
+        "site": "https://developers.google.com/web-toolkit/",
+        "description": "JS in Java."
+      },
+      {
+        "_id": { "$oid": "51873903e4b0072de7e07c3c" },
+        "name": "Ember",
+        "site": "http://emberjs.com/",
+        "description": "Ambitious web apps."
+      },
+      {
+        "_id": { "$oid": "51873903e4b00e8a821a905d" },
+        "name": "Batman",
+        "site": "http://batmanjs.org/",
+        "description": "Quick and beautiful."
+      }
+    ]
+};
